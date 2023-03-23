80a387fcc60f1872df076360deac01f763c471ae
Matheus Diógenes <mathdiogenes@gmail.com>
2023-03-23 12:05:12 -0300

    Car Service findById function test

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -31,7 +31,7 @@ describe('Car Service tests', function () {
     expect(result).to.deep.equal(outputCar);
   });
 
-  it('Tests if it returns a list of cars', async function () {
+  it('Tests if it returns a list of cars with success', async function () {
     const outputList: Car[] = [new Car({
       model: 'Marea',
       year: 2002,
@@ -46,5 +46,23 @@ describe('Car Service tests', function () {
     const carService = new CarService();
     const result = await carService.find();
     expect(result).to.deep.equal(outputList);
+  });
+
+  it('Tests if it returns a car by id with success', async function () {
+    const inputId = '634852326b35b59438fbea2f';
+    const outputCar: Car = new Car({
+      model: 'Marea',
+      year: 1992,
+      color: 'Red',
+      status: true,
+      buyValue: 12.000,
+      doorsQty: 2,
+      seatsQty: 5,
+    });
+    sinon.stub(Model, 'findById').resolves(outputCar);
+
+    const carService = new CarService();
+    const result = await carService.findById(inputId);
+    expect(result).to.deep.equal(outputCar);
   });
 });
