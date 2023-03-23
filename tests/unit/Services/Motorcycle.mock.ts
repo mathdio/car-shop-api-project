043d9b79c1fa6d9e49ba94f7f5fde4dde318d553
Matheus Diógenes <mathdiogenes@gmail.com>
2023-03-23 15:04:03 -0300

    Motorcycle Service find and findById functions tests

---
+++ tests/unit/Services/Motorcycle.mock.ts
@@ -0,0 +1,14 @@
+import Motorcycle from '../../../src/Domains/Motorcycle';
+
+const outputMotorcycle: Motorcycle = new Motorcycle({
+  id: '6348513f34c397abcad040b2',
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+});
+
+export default outputMotorcycle;
--- tests/unit/Services/MotorcycleService.test.ts
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -4,6 +4,7 @@ import sinon from 'sinon';
 import Motorcycle from '../../../src/Domains/Motorcycle';
 import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
 import MotorcycleService from '../../../src/Services/MotorcycleService';
+import outputMotorcycle from './Motorcycle.mock';
 
 describe('Motorcycle Service tests', function () {
   it('Tests if it creates a motorcycle with success', async function () {
@@ -16,20 +17,28 @@ describe('Motorcycle Service tests', function () {
       category: 'Street',
       engineCapacity: 600,
     };
-    const outputMotorcycle: Motorcycle = new Motorcycle({
-      id: '6348513f34c397abcad040b2',
-      model: 'Honda Cb 600f Hornet',
-      year: 2005,
-      color: 'Yellow',
-      status: true,
-      buyValue: 30.000,
-      category: 'Street',
-      engineCapacity: 600,
-    });
     sinon.stub(Model, 'create').resolves(outputMotorcycle);
 
     const motorcycleService = new MotorcycleService();
     const result = await motorcycleService.create(inputMotorcycle);
+    expect(result).to.deep.equal(outputMotorcycle);
+  });
+
+  it('Tests if it returns a motorcycles list with success', async function () {
+    const outputList: Motorcycle[] = [outputMotorcycle];
+    sinon.stub(Model, 'find').resolves(outputList);
+
+    const motorcycleService = new MotorcycleService();
+    const result = await motorcycleService.find();
+    expect(result).to.deep.equal(outputList);
+  });
+
+  it('Tests if it returns a motorcycle by id with success', async function () {
+    const inputId = '6348513f34c397abcad040b2';
+    sinon.stub(Model, 'findById').resolves(outputMotorcycle);
+
+    const motorcycleService = new MotorcycleService();
+    const result = await motorcycleService.findById(inputId);
     expect(result).to.deep.equal(outputMotorcycle);
   });
 });
