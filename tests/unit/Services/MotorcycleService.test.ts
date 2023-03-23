656c73dcf65a6014a3e38bf25c03ef73dbf03480
Matheus Diógenes <mathdiogenes@gmail.com>
2023-03-23 14:47:28 -0300

    Motorcycle Service create function test

---
+++ tests/unit/Services/MotorcycleService.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import { Model } from 'mongoose';
+import sinon from 'sinon';
+import Motorcycle from '../../../src/Domains/Motorcycle';
+import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+import MotorcycleService from '../../../src/Services/MotorcycleService';
+
+describe('Motorcycle Service tests', function () {
+  it('Tests if it creates a motorcycle with success', async function () {
+    const inputMotorcycle: IMotorcycle = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+    const outputMotorcycle: Motorcycle = new Motorcycle({
+      id: '6348513f34c397abcad040b2',
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30.000,
+      category: 'Street',
+      engineCapacity: 600,
+    });
+    sinon.stub(Model, 'create').resolves(outputMotorcycle);
+
+    const motorcycleService = new MotorcycleService();
+    const result = await motorcycleService.create(inputMotorcycle);
+    expect(result).to.deep.equal(outputMotorcycle);
+  });
+});
